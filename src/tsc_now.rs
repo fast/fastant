e96c833da2fb466e1c657a4a1c6f56c9938f3163
Yunze Xu <xyzinfernity@163.com>
2026-05-12 21:15:32 +0800

    fix clippy check

--- src/tsc_now.rs
+++ src/tsc_now.rs
@@ -136,7 +136,7 @@ fn has_invariant_tsc() -> bool {
     use core::arch::x86_64::__cpuid;
 
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-    unsafe {
+    {
         let cpuid_invariant_tsc_bts = 1 << 8;
         __cpuid(0x80000000).eax >= 0x80000007
             && __cpuid(0x80000007).edx & cpuid_invariant_tsc_bts != 0
